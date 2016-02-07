a2a9fb409f5cf4eaec23154564c25b64e8b4fca0
Jesus E. Larios Murillo <preparedfortherain@gmail.com>
2016-02-07 00:21:32 -0800

    AddEventActivity Skeleton

---
+++ app/src/main/java/com/cse403/matchonthestreet/AddEventActivity.java
@@ -0,0 +1,18 @@
+package com.cse403.matchonthestreet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by larioj on 2/7/16.
+ */
+public class AddEventActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event);
+
+        // add other stuff
+    }
+}
