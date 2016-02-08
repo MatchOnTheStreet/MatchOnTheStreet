9c7b43c61cca87af505cc3b229247a3e5b800e3f
Iris Wang <Iris@D-69-91-176-11.dhcp4.washington.edu>
2016-02-07 16:09:19 -0800

    update login

--- app/src/main/java/com/cse403/matchonthestreet/DBManager.java
+++ app/src/main/java/com/cse403/matchonthestreet/DBManager.java
@@ -14,7 +14,29 @@ public class DBManager {
     static final String USER = "ruijiw@c-24-16-210-83.hsd1.wa.comcast.net";
     static final String PASS = "bZQf23xm";
 
+    private String uid;
+
+    private static final String USER_LOGIN_SQL =
+            "SELECT * FROM Account WHERE identifier = ?";
+    private PreparedStatement userLoginStatement;
+
+    private static final String CREATE_ACCOUNT_SQL =
+            "INSERT INTO Account VALUES(?,?)";
+    private PreparedStatement createAccountStatement;
+
+    // transactions
+    private static final String BEGIN_TRANSACTION_SQL =
+            "SET TRANSACTION ISOLATION LEVEL SERIALIZABLE; BEGIN TRANSACTION";
+    private PreparedStatement beginTransactionStatement;
+
+    private static final String COMMIT_SQL = "COMMIT TRANSACTION";
+    private PreparedStatement commitTransactionStatement;
+
+    private static final String ROLLBACK_SQL = "ROLLBACK TRANSACTION";
+    private PreparedStatement rollbackTransactionStatement;
+
     private Connection conn;
+
     /* Connection code to MySQL.  */
     public void openConnection() throws Exception {
         Class.forName("com.mysql.jdbc.Driver");
@@ -26,4 +48,56 @@ public class DBManager {
         conn.close();
     }
 
+    /* prepare all the SQL statements in this method.
+      "preparing" a statement is almost like compiling it. */
+
+    public void prepareStatements() throws Exception {
+        beginTransactionStatement = conn.prepareStatement(BEGIN_TRANSACTION_SQL);
+        commitTransactionStatement = conn.prepareStatement(COMMIT_SQL);
+        rollbackTransactionStatement = conn.prepareStatement(ROLLBACK_SQL);
+        userLoginStatement = conn.prepareStatement(USER_LOGIN_SQL);
+        createAccountStatement = conn.prepareStatement(CREATE_ACCOUNT_SQL);
+
+    }
+
+    public void transaction_login(String identifier) throws Exception {
+        userLoginStatement.clearParameters();
+        userLoginStatement.setString(1,identifier);
+        ResultSet uid_set = userLoginStatement.executeQuery();
+        if (uid_set.next()){
+            this.uid = uid_set.getString(1);
+            System.out.println("Successfully login.");
+        } else {
+            beginTransaction();
+            try {
+                createAccountStatement.clearParameters();
+                createAccountStatement.setString(1, identifier);
+                createAccountStatement.setString(2, null);
+                createAccountStatement.execute();
+                System.out.println("Create a new account.");
+                commitTransaction();
+            } catch (SQLException e) {
+                try {
+                    rollbackTransaction();
+                } catch (Exception rollbackE) {
+                    System.out.println("Error when roll back!");
+                }
+            }
+        }
+        uid_set.close();
+    }
+
+    public void beginTransaction() throws Exception {
+        conn.setAutoCommit(false);
+        beginTransactionStatement.executeUpdate();
+    }
+
+    public void commitTransaction() throws Exception {
+        commitTransactionStatement.executeUpdate();
+        conn.setAutoCommit(true);
+    }
+    public void rollbackTransaction() throws Exception {
+        rollbackTransactionStatement.executeUpdate();
+        conn.setAutoCommit(true);
+    }
 }
