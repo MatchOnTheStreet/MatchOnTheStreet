cc7d5b0a846a5f81659df14e0106749e0c4769e6
bentebbs <ben@thetebbs.com>
2016-02-07 23:51:23 -0800

    added simple login page, hooked up fb api to app

---
+++ app/src/main/java/com/cse403/matchonthestreet/LoginActivity.java
@@ -0,0 +1,57 @@
+package com.cse403.matchonthestreet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import com.facebook.login.widget.LoginButton;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+
+/**
+ * Simple facebook login page
+ */
+public class LoginActivity extends Activity {
+
+    private CallbackManager callbackManager;
+    private TextView info;
+    private LoginButton loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
+        setContentView(R.layout.activity_login);
+        info = (TextView)findViewById(R.id.info);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                info.setText("User ID:  " + loginResult.getAccessToken().getUserId());
+            }
+
+            @Override
+            public void onCancel() {
+                info.setText("Login attempt cancelled.");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                info.setText("Login attempt failed.");
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
