76cfd8b8ebba0af4796b84f37932738efba8962e
bentebbs <ben@thetebbs.com>
2016-02-08 17:18:42 -0800

    some comments

--- app/src/main/java/com/cse403/matchonthestreet/Account.java
+++ app/src/main/java/com/cse403/matchonthestreet/Account.java
@@ -4,10 +4,18 @@ import java.util.List;
 
 /**
  * Created by Iris on 2/7/16.
+ *
+ * Represents a user's account.
  */
 public class Account {
+
+    // User ID given by facebook.
     public String identifier;
+
+    // The name of the user whose account this is.
     public String name;
+
+    // A list of events the user is planning on attending.
     public List<Event> events;
 
     public Account(String identifier) {
--- app/src/main/java/com/cse403/matchonthestreet/Event.java
+++ app/src/main/java/com/cse403/matchonthestreet/Event.java
@@ -6,12 +6,23 @@ import java.util.*;
 
 /**
  * Created by Iris on 2/7/16.
+ *
+ * Represents a sporting Event.
  */
 public class Event {
+    // The title of the event
     public String title;
+
+    // Where the event is held.
     public Location location;
+
+    // The time of the event
     public Date time;
+
+    // A description of the event.
     public String description;
+
+    // A list of accounts who have said they will be attending the event.
     public List<Account> attending;
 
     public Event(String title, Location location, Date time, String description) {
