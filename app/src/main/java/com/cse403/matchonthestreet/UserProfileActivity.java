5c5c778bf45823fc20db8617e071575122eaf4ce
bentebbs <ben@thetebbs.com>
2016-02-08 16:54:38 -0800

    should have created account page, needs button

--- app/src/main/java/com/cse403/matchonthestreet/UserProfileActivity.java
+++ app/src/main/java/com/cse403/matchonthestreet/UserProfileActivity.java
@@ -2,12 +2,28 @@ package com.cse403.matchonthestreet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class UserProfileActivity extends AppCompatActivity {
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        listView = (ListView) findViewById(android.R.id.list);
+
+        String[] sampleVal = new String[]{"Tennis match @ Denny",
+                "Casual pool play",
+                "Team Potato needs a goalkeeper",
+                "Basket ball IMA 5v5",
+                "Tennis match @ Denny"};
+
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.activity_list_item, android.R.id.text1);
+
+        listView.setAdapter(listAdapter);
     }
 }
