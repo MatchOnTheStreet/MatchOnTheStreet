65eb51fd41e96a84572fd00dad50c3180b6fe4c4
Jesus E. Larios Murillo <preparedfortherain@gmail.com>
2016-02-19 16:12:33 -0800

    added db stuff.

--- app/src/main/java/com/cse403/matchonthestreet/DBManager.java
+++ app/src/main/java/com/cse403/matchonthestreet/DBManager.java
@@ -11,13 +11,78 @@ import java.util.List;
  * Created by Iris on 2/7/16.
  */
 public class DBManager {
-    // JDBC driver name and database URL
+    // Database Credentials
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://matchonthestreetdb.crqizzvrxges.us-east-1.rds.amazonaws.com:3306/motsdb";
-
-    //  Database credentials
     static final String USER = "larioj";
     static final String PASS = "motspassword";
+
+    private Connection conn;
+
+    // SQL Queries
+    private static final String GET_ACCOUNT_BY_UID =
+            "select * from Accounts where uid = ?;";
+    private static final String GET_EVENT_BY_ID =
+            "select * from Events where eid = ?;";
+    private static final String ADD_EVENT =
+            "INSERT INTO Events (title, longitude, latitude, time, description) VALUES (?,?,?,?,?);";
+    private  static final String ADD_ACCOUNT =
+            "insert into Accounts (uid, name) values (?, ?);";
+
+
+    public void addEventWithParams(String title, Double longitude,
+                                   Double latitude, Timestamp datetime, String description)
+            throws SQLException, ClassNotFoundException {
+        openConnection();
+        PreparedStatement addEventStatement = conn.prepareStatement(ADD_EVENT);
+        addEventStatement.clearParameters();
+        addEventStatement.setString(1, title);
+        addEventStatement.setDouble(2, longitude);
+        addEventStatement.setDouble(3, latitude);
+        addEventStatement.setTimestamp(4, datetime);
+        addEventStatement.setString(5, description);
+        addEventStatement.executeUpdate();
+        closeConnection();
+    }
+
+    public void addAccountWithParams(String uid, String name)
+            throws SQLException, ClassNotFoundException {
+        openConnection();
+        PreparedStatement addEventStatement = conn.prepareStatement(ADD_ACCOUNT);
+        addEventStatement.clearParameters();
+        addEventStatement.setString(1, uid);
+        addEventStatement.setString(2, name);
+        addEventStatement.executeUpdate();
+        closeConnection();
+    }
+
+    public Event getEventWithParams(int eid)
+            throws SQLException, ClassNotFoundException {
+        openConnection();
+        PreparedStatement addEventStatement = conn.prepareStatement(GET_EVENT_BY_ID);
+        addEventStatement.clearParameters();
+        addEventStatement.setInt(1, eid);
+        ResultSet eventSet = addEventStatement.executeQuery();
+        closeConnection();
+
+        String title;
+        Double longitude;
+        Double latitude;
+        Timestamp datetime;
+        String description;
+
+        if (eventSet.next()) {
+            title = eventSet.getString("title");
+            longitude= eventSet.getDouble("logitude");
+            latitude = eventSet.getDouble("latitude");
+            datetime = eventSet.getTimestamp("time");
+
+        }
+        return null;
+    }
+
+
+
 
     private String uid;
 
@@ -30,7 +95,7 @@ public class DBManager {
     private PreparedStatement createAccountStatement;
 
     private static final String ADD_EVENT_SQL =
-            "INSERT INTO Event VALUES(?,?,?,?,?)";
+            "INSERT INTO Events VALUES(?,?,?,?,?)";
     private PreparedStatement addEventStatement;
 
     private static final String GET_EVENT_SQL =
@@ -47,11 +112,11 @@ public class DBManager {
     private static final String ROLLBACK_SQL = "ROLLBACK TRANSACTION";
     private PreparedStatement rollbackTransactionStatement;
 
-    private Connection conn;
+
 
     /* Connection code to MySQL.  */
     public void openConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
+        Class.forName(JDBC_DRIVER);
         System.out.println("Connecting to database...");
         conn=DriverManager.getConnection(DB_URL,USER,PASS);
     }
@@ -73,6 +138,7 @@ public class DBManager {
         getEventStatement = conn.prepareStatement(GET_EVENT_SQL);
 
     }
+
 
     public void transaction_login(String uid, String name) throws Exception {
         userLoginStatement.clearParameters();
