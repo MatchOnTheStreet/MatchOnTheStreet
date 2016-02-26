a6265d38b5e7ae4c3ff228037b635ef115a54bd6
bentebbs <ben@thetebbs.com>
2016-02-25 22:32:16 -0800

    Events data on MyEvents dynamically loading

--- app/src/main/java/com/cse403/matchonthestreet/view/UserProfileActivity.java
+++ app/src/main/java/com/cse403/matchonthestreet/view/UserProfileActivity.java
@@ -1,7 +1,10 @@
 package com.cse403.matchonthestreet.view;
 
 import android.content.Intent;
+import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -9,13 +12,17 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.cse403.matchonthestreet.R;
+import com.cse403.matchonthestreet.backend.DBManager;
+import com.cse403.matchonthestreet.controller.MOTSApp;
 import com.cse403.matchonthestreet.models.Account;
 import com.cse403.matchonthestreet.models.Event;
 import com.facebook.FacebookSdk;
 import com.facebook.Profile;
 import com.facebook.login.LoginManager;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -65,15 +72,54 @@ public class UserProfileActivity extends NavActivity {
             username.setText("null username");
         }
 
+        AsyncTask<Integer, Integer, List<Event>> task = new AsyncTask<Integer, Integer, List<Event>>() {
+            @Override
+            protected List<Event> doInBackground(Integer[] params) {
+                Log.d(TAG, "here");
+                try {
+                    Account account = ((MOTSApp) getApplication()).getMyAccount();
+                    List<Event> events = DBManager.getEventsAttendedByAccount(account);
+                    Log.d(TAG, account.getName());
+                    if (events.isEmpty()) {
+                        Log.d(TAG, "empty events");
+                    } else {
+                        for (Event e : events) {
+                            Log.d(TAG, e.title);
+                        }
+                    }
+                    return events;
+                } catch (ClassNotFoundException e) {
+                    Log.d(TAG, "ClassNotFoundException");
+                    e.printStackTrace();
+                } catch (SQLException e) {
+                    Log.d(TAG, "SQL Exception");
+                    e.printStackTrace();
+                }
+                return null;
+            }
 
-        List<Event> eventsAttending = dummyAccount.getEvents();
-        List<String> sampleVals = new ArrayList<String>();
-        for (Event e : eventsAttending) {
-            sampleVals.add(e.title + ": " + e.description);
-        }
+            protected void onPostExecute(List<Event> events) {
+                Log.d(TAG, "On Post Execute");
+                if (events != null) {
+                    List<String> vals = new ArrayList<String>();
+                    for (Event e : events) {
+                        vals.add(e.title + ": " + e.description);
+                        addEventsToList(vals);
 
-        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, sampleVals);
+                    }
+                }
 
+            }
+
+        };
+        task.execute();
+    }
+
+
+
+    private void addEventsToList(List<String> vals) {
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, vals);
         listView.setAdapter(listAdapter);
+
     }
 }
