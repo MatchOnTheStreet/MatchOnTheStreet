26bc7af2430e8c2820cf17a50f8e47bd2c4b28b4
Jesus E. Larios Murillo <preparedfortherain@gmail.com>
2016-02-25 16:56:23 -0800

    added function that were removed

--- app/src/main/java/com/cse403/matchonthestreet/backend/DBManager.java
+++ app/src/main/java/com/cse403/matchonthestreet/backend/DBManager.java
@@ -29,17 +29,17 @@ public final class DBManager {
     private static final String CREATE_ACCOUNT_SQL =
             "INSERT INTO Accounts (uid, name) VALUES(?,?);";
 
-    private  static final String ADD_ACCOUNT_TO_EVENT_SQL =
+    private static final String ADD_ACCOUNT_TO_EVENT_SQL =
             "INSERT INTO Attending (uid, eid) VALUES (?, ?);";
 
     private static final String ADD_EVENT_SQL =
             "INSERT INTO Events "
-                + "(eid, title, longitude, latitude, start_time, duration, time_created, description) "
-                + "VALUES (?,?,?,?,?,?,?,?);";
+                    + "(eid, title, longitude, latitude, start_time, duration, time_created, description) "
+                    + "VALUES (?,?,?,?,?,?,?,?);";
 
     private static final String GET_EVENTS_IN_RADIUS_SQL =
             "SELECT * FROM Events e WHERE e.latitude < ? AND e.latitude > ? "
-            + "AND e.longitude < ? AND e.longitude > ?;";
+                    + "AND e.longitude < ? AND e.longitude > ?;";
 
     private static final String GET_EVENT_BY_ID_SQL =
             "SELECT * FROM Events WHERE eid = ?;";
@@ -49,11 +49,21 @@ public final class DBManager {
 
     private static final String GET_EVENTS_ATTENDED_BY_ACCOUNT_SQL
             = "SELECT e.eid AS eid, e.title AS title, e.longitude AS longitude, "
-                    + "e.latitude AS latitude, e.start_time AS start_time, "
-                    + "e.duration AS duration, e.time_created AS time_created, "
-                    + "e.description AS description "
+            + "e.latitude AS latitude, e.start_time AS start_time, "
+            + "e.duration AS duration, e.time_created AS time_created, "
+            + "e.description AS description "
             + "FROM Events e, Attending a"
             + "WHERE a.uid = ? AND a.eid = e.eid";
+
+    private static final String GET_ACCOUNTS_ATTENDING_EVENT_SQL
+            = "SELECT a.uid AS uid, a.name AS name "
+            + "FROM Accounts a, Attending at "
+            + "WHERE at.eid = ? AND at.uid = a.uid;";
+
+    private static final String GET_COUNT_OF_ACCOUNTS_ATTENDING_EVENT_SQL
+            = "SELECT COUNT(*) "
+            + "FROM Accounts a, Attending at "
+            + "WHERE at.eid = ? AND at.uid = a.uid;";
 
     /* transactions */
     private static final String BEGIN_TRANSACTION_SQL =
@@ -67,7 +77,7 @@ public final class DBManager {
         // Nothing
     }
 
-   // Methods
+    // Methods
 
     public static void addEvent(Event event) throws SQLException, ClassNotFoundException {
         Connection conn = openConnection();
@@ -120,7 +130,6 @@ public final class DBManager {
         return null;
     }
 
-
     public static List<Event> getEventsInRadius(Location location, double radius) throws SQLException, ClassNotFoundException {
         Connection conn = openConnection();
         PreparedStatement getEventByRadiusStatement = conn.prepareStatement(GET_EVENTS_IN_RADIUS_SQL);
@@ -132,7 +141,7 @@ public final class DBManager {
         ResultSet rs = getEventByRadiusStatement.executeQuery();
         closeConnection(conn);
         List<Event> list = new ArrayList<>();
-        while (rs.next()){
+        while (rs.next()) {
             int eid = rs.getInt("eid");
             String title = rs.getString("title");
             Location loc = new Location("new location");
@@ -175,6 +184,7 @@ public final class DBManager {
         getUserEventStatement.clearParameters();
         getUserEventStatement.setInt(1, account.getUid());
         ResultSet rs = getUserEventStatement.executeQuery();
+        closeConnection(conn);
         while (rs.next()) {
             int eid = rs.getInt("eid");
             String title = rs.getString("title");
@@ -191,14 +201,42 @@ public final class DBManager {
         return list;
     }
 
+    public static int getCountOfAccountsAttendingEvent(Event event) throws SQLException, ClassNotFoundException {
+        Connection conn = openConnection();
+        PreparedStatement st = conn.prepareStatement(GET_COUNT_OF_ACCOUNTS_ATTENDING_EVENT_SQL);
+        int count = 0;
+        st.clearParameters();
+        st.setInt(1, event.eid);
+        ResultSet rs = st.executeQuery();
+        if (rs.next()) {
+            count = rs.getInt(1);
+        }
+        return count;
+    }
+
+    public static List<Account> getAccountsAttendingEvent(Event event) throws SQLException, ClassNotFoundException {
+        Connection conn = openConnection();
+        PreparedStatement st = conn.prepareStatement(GET_ACCOUNTS_ATTENDING_EVENT_SQL);
+        List<Account> list = new ArrayList<>();
+        st.clearParameters();
+        st.setInt(1, event.eid);
+        ResultSet rs = st.executeQuery();
+        while (rs.next()) {
+            int uid = rs.getInt("uid");
+            String name = rs.getString("name");
+            Account account = new Account(uid, name);
+            list.add(account);
+        }
+        return list;
+    }
+
     /* Connection code to MySQL.  */
-    public static Connection openConnection() throws ClassNotFoundException, SQLException {
+    private static Connection openConnection() throws ClassNotFoundException, SQLException {
         Class.forName(JDBC_DRIVER);
-        System.out.println("Connecting to database...");
         return DriverManager.getConnection(DB_URL, USER, PASS);
     }
 
-    public static void closeConnection(Connection conn) throws SQLException {
+    private static void closeConnection(Connection conn) throws SQLException {
         conn.close();
     }
 
