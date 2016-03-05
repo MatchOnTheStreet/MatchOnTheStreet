4ac63a1aa131665ad10634f580a19e2d693393a3
bentebbs <ben@thetebbs.com>
2016-03-04 22:42:49 -0800

    Updated tests for some activities. Does not work with Marshmallow. Added LoginActivityTest

--- app/src/androidTest/java/com/cse403/matchonthestreet/AddEventActivityTest.java
+++ app/src/androidTest/java/com/cse403/matchonthestreet/AddEventActivityTest.java
@@ -10,7 +10,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasFocus;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isSelected;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
@@ -23,5 +28,13 @@ public class AddEventActivityTest {
 
     @Test
     public void shouldBeAbleToLaunchScreen() {
+        onView(withId(R.id.event_title)).check(matches(isDisplayed()));
+        onView(withId(R.id.event_from_row)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testClickEventField() {
+        onView(withId(R.id.event_from_row)).perform(click());
+        onView(withId(R.id.event_from_row)).check(matches(hasFocus()));
     }
 }
--- app/src/androidTest/java/com/cse403/matchonthestreet/LoginActivityTest.java
+++ app/src/androidTest/java/com/cse403/matchonthestreet/LoginActivityTest.java
@@ -1,7 +1,9 @@
 package com.cse403.matchonthestreet;
 
+import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.cse403.matchonthestreet.view.LoginActivity;
 import com.cse403.matchonthestreet.view.MapsActivity;
 
 import org.junit.Rule;
@@ -11,18 +13,22 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by larioj on 2/26/16.
  */
 @RunWith(AndroidJUnit4.class)
-public class MapsActivityTest {
+public class LoginActivityTest {
     @Rule
-    public final ActivityRule<MapsActivity> main = new ActivityRule<>(MapsActivity.class);
+    public final ActivityRule<LoginActivity> main = new ActivityRule<>(LoginActivity.class);
 
     @Test
     public void shouldBeAbleToLaunchScreen() {
-        onView(withId(R.id.fab_map_to_list)).check(matches(isDisplayed()));
+        onView(withId(R.id.login_background)).check(matches(isDisplayed()));
+        onView(withId(R.id.text_welcome)).check(matches(isDisplayed()));
+
     }
 }
